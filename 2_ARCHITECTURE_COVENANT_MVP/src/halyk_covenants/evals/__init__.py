7ed1f488c9db118866262e2e5e331e941e9b2cad
Alexloroo <voronkoleha00@gmail.com>
2026-08-03 00:39:18 +0500

    feat: export LangSmith exact evaluators

--- 2_ARCHITECTURE_COVENANT_MVP/src/halyk_covenants/evals/__init__.py
+++ 2_ARCHITECTURE_COVENANT_MVP/src/halyk_covenants/evals/__init__.py
@@ -1,3 +1,10 @@
+from halyk_covenants.evals.langsmith import (
+    covenant_result_evaluators,
+    evidence_exact_evaluator,
+    full_exact_match_evaluator,
+    number_exact_evaluator,
+    verdict_exact_evaluator,
+)
 from halyk_covenants.evals.scoring import (
     score_compiler_output,
     score_covenant_detection,
@@ -5,7 +12,12 @@ from halyk_covenants.evals.scoring import (
 )
 
 __all__ = [
+    "covenant_result_evaluators",
+    "evidence_exact_evaluator",
+    "full_exact_match_evaluator",
+    "number_exact_evaluator",
     "score_compiler_output",
     "score_covenant_detection",
     "score_covenant_result",
+    "verdict_exact_evaluator",
 ]
