36919826ed902972b4650e4fbb457c87d720e314
Alexloroo <voronkoleha00@gmail.com>
2026-08-03 00:36:27 +0500

    feat: expose component-level evaluators

---
+++ 2_ARCHITECTURE_COVENANT_MVP/src/halyk_covenants/evals/__init__.py
@@ -0,0 +1,11 @@
+from halyk_covenants.evals.scoring import (
+    score_compiler_output,
+    score_covenant_detection,
+    score_covenant_result,
+)
+
+__all__ = [
+    "score_compiler_output",
+    "score_covenant_detection",
+    "score_covenant_result",
+]
