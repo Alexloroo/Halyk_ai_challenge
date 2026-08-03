e96ee5a36429cdb373f235c54913858566cc9448
Alexloroo <voronkoleha00@gmail.com>
2026-08-03 15:53:42 +0500

    feat: export review pipeline

--- 2_ARCHITECTURE_COVENANT_MVP/src/halyk_covenants/pipeline/__init__.py
+++ 2_ARCHITECTURE_COVENANT_MVP/src/halyk_covenants/pipeline/__init__.py
@@ -1,9 +1,12 @@
 from .evaluate import BatchEvaluationPipeline, BatchEvaluationReport
 from .preprocess import PreprocessPipeline, PreprocessReport
+from .review import ReviewPipeline, ReviewedBatchReport
 
 __all__ = [
     "BatchEvaluationPipeline",
     "BatchEvaluationReport",
     "PreprocessPipeline",
     "PreprocessReport",
+    "ReviewPipeline",
+    "ReviewedBatchReport",
 ]
